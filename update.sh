2f07c93eb966d94ec20f0dda5e50bcf58633d0ed
scout <kashdasjkhkk@asjdjaskh.aldsasj>
2026-06-09 12:47:08 -0600

    added update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # exit on any error
+
+echo "Updating..."
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+git pull
+
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "Done."
